1b262d5755468df959a4e404abfbb410613932dd
Jason Keung <jason.keungg@gmail.com>
2019-08-11 14:29:14 -0700

    Add scripts to package.json

---
+++ build/precache-manifest.a86c22fecb015b22ea024462b446a5a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0853721e5ec41311ef45000269dc0d",
+    "url": "/feedback-form/index.html"
+  },
+  {
+    "revision": "9b19fce8bcc2dff0b345",
+    "url": "/feedback-form/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1f04c09f2293dade0051",
+    "url": "/feedback-form/static/css/main.f973da19.chunk.css"
+  },
+  {
+    "revision": "9b19fce8bcc2dff0b345",
+    "url": "/feedback-form/static/js/2.5db36e52.chunk.js"
+  },
+  {
+    "revision": "1f04c09f2293dade0051",
+    "url": "/feedback-form/static/js/main.c3256cfc.chunk.js"
+  },
+  {
+    "revision": "c6f812491f7dc628c938",
+    "url": "/feedback-form/static/js/runtime~main.f3664027.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a495ab3661516c2acc805655f4fd8971.js"
+  "/feedback-form/precache-manifest.a86c22fecb015b22ea024462b446a5a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/feedback-form/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
